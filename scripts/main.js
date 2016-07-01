92c0148c0ea80b4a7ebc53e31c3e4ebb537fd668
dargains <andre.dargains@gmail.com>
2016-07-01 23:05:28 +0100

    header

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $(function () {
         $("header").load("modules/header.html");
         $(".hero").load("modules/hero.html");
+        $(".teste").load("modules/teste.html");
         //$("footer").load("modules/footer.html");
     });
 });
